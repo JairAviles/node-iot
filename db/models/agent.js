56af5749506787970ab1eaa697f5ab4952a6f9fd
Jair Avilés <hi@jairaviles.mx>
2019-09-30 17:43:22 -0700

    Decouple models definition

--- db/models/agent.js
+++ db/models/agent.js
@@ -35,22 +35,4 @@ const setupAgentModel = (config) => {
   })
 }
 
-const setupMetricModel = (config) => {
-  const sequelize = setupDatabase(config)
-
-  return sequelize.define('agent', {
-    type: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    value: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    }
-  })
-}
-
-module.exports = {
-  setupAgentModel,
-  setupMetricModel
-}
+module.exports = setupAgentModel
